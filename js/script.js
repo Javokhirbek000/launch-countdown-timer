3fc0bf65fed10580ce40409bfa0b2a3566a3c163
Javoxir Xolmirzayev <javoxirxolmirzayev5@gmail.com>
2024-04-02 00:24:19 +0500

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,58 @@
-const dedline = "2024-04-16";
+const deadline = "2024-04-16";
 
-console.log(Date.parse(dedline))
-console.log(new Date(dedline))
+function getRemainingTime(dl) {
+  let time = Date.parse(new Date(dl)) - Date.parse(new Date())
+  let days = Math.floor(time / (1000 * 60 * 60 * 24))
+  let hours = Math.floor(time / (1000 * 60 * 60) % 24)
+  let minutes = Math.floor(time / (1000 * 60) % 60)
+  let seconds = Math.floor((time / 1000) % 60)
+
+  if (time < 0) {
+    time = 0
+    days = 0
+    hours = 0
+    minutes = 0
+    seconds = 0
+  }
+  return {
+    time: time,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds
+  }
+}
+
+function setTimerInfo(dl) {
+  let daysEl = document.querySelector('.days')
+  let hoursEl = document.querySelector('.hours')
+  let minutesEl = document.querySelector('.minutes')
+  let secondsEl = document.querySelector('.seconds')
+
+  let timerId = setInterval(updateTimer, 1000)
+
+  updateTimer()
+
+  function updateTimer() {
+    let t = getRemainingTime(dl);
+
+    daysEl.textContent = addZero(t.days);
+    hoursEl.textContent = addZero(t.hours);
+    minutesEl.textContent = addZero(t.minutes);
+    secondsEl.textContent = addZero(t.seconds);
+
+    if (t.time <= 0) {
+      clearInterval(timerId)
+    }
+  }
+}
+
+function addZero(num) {
+  if (num >= 0 && num < 10) {
+    return `0${num}`
+  } else {
+    return num
+  }
+}
+
+setTimerInfo(deadline)
